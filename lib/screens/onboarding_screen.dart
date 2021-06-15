b84fa2b742342317e43bf95764d36ca5e051fc39
aadesh1214 <aadesh.parija999@gmail.com>
2021-06-15 16:30:50 +0530

    updated onboading_screen.dart

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -8,43 +8,56 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
-
-
   var pages = [
     PageViewModel(
-      title: "Title of first page",
-      body: "Here you can write the description of the page, to explain someting...",
+      title: "Learn",
+      body: "You can learn...",
       image: Center(child: Image.asset("assets/screen1.png", height: 175.0)),
       decoration: const PageDecoration(
         pageColor: Color(0xFF192A56),
-        bodyTextStyle: TextStyle(color: Colors.white, fontSize: 16,),
-        titleTextStyle: TextStyle(color: Colors.white, fontSize: 24,),
+        bodyTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 16,
+        ),
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 24,
+        ),
       ),
     ),
-
     PageViewModel(
-      title: "Title of first page",
-      body: "Here you can write the description of the page, to explain someting...",
+      title: "Leap",
+      body: "You can leap and if you don't giveup...",
       image: Center(child: Image.asset("assets/screen2.png", height: 175.0)),
       decoration: const PageDecoration(
         pageColor: Color(0xFF192A56),
-        bodyTextStyle: TextStyle(color: Colors.white, fontSize: 16,),
-        titleTextStyle: TextStyle(color: Colors.white, fontSize: 24,),
+        bodyTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 16,
+        ),
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 24,
+        ),
       ),
     ),
-
     PageViewModel(
-      title: "Title of first page",
-      body: "Here you can write the description of the page, to explain someting...",
+      title: "Lead",
+      body: "You can become perfect and can lead to others...",
       image: Center(child: Image.asset("assets/screen3.png", height: 175.0)),
       decoration: const PageDecoration(
         pageColor: Color(0xFF192A56),
-        bodyTextStyle: TextStyle(color: Colors.white, fontSize: 16,),
-        titleTextStyle: TextStyle(color: Colors.white, fontSize: 24,),
+        bodyTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 16,
+        ),
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 24,
+        ),
       ),
     ),
   ];
-
 
   @override
   Widget build(BuildContext context) {
@@ -53,34 +66,40 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
         pages: pages,
         onDone: () {
           Navigator.pushReplacement(
-            context, 
-            MaterialPageRoute(
-              builder: (context) => HomePage()
-            ),
+            context,
+            MaterialPageRoute(builder: (context) => HomePage()),
           );
         },
         onSkip: () {
           Navigator.pushReplacement(
-            context, 
-            MaterialPageRoute(
-              builder: (context) => HomePage()
-            ),
+            context,
+            MaterialPageRoute(builder: (context) => HomePage()),
           );
         },
         showSkipButton: true,
-        skip: Icon(Icons.skip_next, color: Colors.white,),
-        next: Icon(Icons.arrow_right, color: Colors.white,),
-        done: const Text("Done", style: TextStyle(fontWeight: FontWeight.w600, color: Colors.white,),),
-        dotsDecorator: DotsDecorator(
-          size: const Size.square(10.0),
-          activeSize: const Size(20.0, 10.0),
-          activeColor: Colors.deepOrange,
-          color: Colors.black26,
-          spacing: const EdgeInsets.symmetric(horizontal: 3.0),
-          activeShape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(25.0)
-          )
+        skip: Icon(
+          Icons.skip_next,
+          color: Colors.white,
         ),
+        next: Icon(
+          Icons.arrow_right,
+          color: Colors.white,
+        ),
+        done: const Text(
+          "Done",
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+          ),
+        ),
+        dotsDecorator: DotsDecorator(
+            size: const Size.square(10.0),
+            activeSize: const Size(20.0, 10.0),
+            activeColor: Colors.deepOrange,
+            color: Colors.black26,
+            spacing: const EdgeInsets.symmetric(horizontal: 3.0),
+            activeShape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25.0))),
       ),
     );
   }
